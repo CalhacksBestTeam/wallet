25cd126c07241060c63e7a349fad545a96be2275
kiwi0401 <kiwi04011@gmail.com>
2022-10-16 13:54:35 -0700

    redeploy

--- pages/api/setNFCInfo.ts
+++ pages/api/setNFCInfo.ts
@@ -32,6 +32,7 @@ export default async function handler(req : any, res : any) {
 
     if(req.method === "DELETE") {
         nfcInfo = "";
+
         res.status(200).json(JSON.stringify(nfcInfo));
         return;
     }
