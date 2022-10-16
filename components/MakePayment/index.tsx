5f396ddc26e8793247701466be5801e1e1a6b44f
kiwi0401 <kiwi04011@gmail.com>
2022-10-16 15:13:27 -0700

    y

--- components/MakePayment/index.tsx
+++ components/MakePayment/index.tsx
@@ -37,7 +37,6 @@ const CreateAccount = (props: { setShow: Function }): ReactElement => {
                 Send Sol to an address of your choice.
             </p>
 
-
             <div className={"buttons"}>
                 <Button type="primary" onClick={handleGenerate}>
                     Connect NFC
