382ab7ad2a47d7a1c2ec8652aa519e6e8712395c
kiwi0401 <kiwi04011@gmail.com>
2022-10-16 13:49:21 -0700

    delete dfn

--- pages/api/setNFCInfo.ts
+++ pages/api/setNFCInfo.ts
@@ -32,8 +32,10 @@ export default async function handler(req : any, res : any) {
 
     if(req.method === "DELETE") {
         nfcInfo = "";
-        return res.status(200).json(JSON.stringify(nfcInfo));
+        res.status(200).json(JSON.stringify(nfcInfo));
+        return;
     }
+
     nfcInfo = req.body;
     res.status(200).json(JSON.stringify(nfcInfo))
 }
