2cce34c029dd7c02b2f158ba704a52b983c15c5b
kiwi0401 <kiwi04011@gmail.com>
2022-10-25 18:37:42 -0700

    websocket

---
+++ pages/api/websocket.ts
@@ -0,0 +1,23 @@
+import { Server } from 'Socket.IO'
+
+const SocketHandler = (req : any, res : any) => {
+    console.log("YO")
+
+    if (res.socket.server.io) {
+        console.log('Socket is already running')
+    } else {
+        console.log('Socket is initializing')
+        const io = new Server(res.socket.server)
+
+        io.sockets.on('connection', (socket : any) => {
+            socket.on('message', (data : any) => {
+                console.log(data)
+                socket.emit("message", data)
+            })
+        })
+        res.socket.server.io = io
+    }
+    res.end()
+}
+
+export default SocketHandler
